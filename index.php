7216f63e2ca91f7caac387073d4dea066ffe8a16
frostkom <martin.fors@frostkom.se>
2025-09-29 09:20:57 +0200

    Change to uninstall user meta

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF BankID
 Plugin URI: https://github.com/frostkom/mf_bank_id
 Description: Extension to use BankID
-Version: 2.7.24
+Version: 2.7.25
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
@@ -82,7 +82,7 @@ if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') &
 		mf_uninstall_plugin(array(
 			'uploads' => $obj_bank_id->post_type,
 			'options' => array('setting_bank_id_certificate', 'option_bank_id_certificate', 'setting_bank_id_certificate_expiry_date', 'setting_bank_id_activate', 'setting_bank_id_login_methods', 'setting_bank_id_login_fields', 'setting_bank_id_api_mode', 'setting_bank_id_login_intent', 'setting_bank_id_sign_intent'),
-			'meta' => array('profile_ssn'),
+			'user_meta' => array('profile_ssn'),
 		));
 	}
 }
